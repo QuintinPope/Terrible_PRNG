6c3204e83caf4edf7e1545b121ca0bca9b8b1d56
QuintinPope <60300243+QuintinPope@users.noreply.github.com>
2021-11-17 20:53:48 -0800

    Create terrible_prng.py

---
+++ terrible_prng.py
@@ -0,0 +1,26 @@
+# Generate some pseudorandom numbers based on three seed values.
+import random
+
+# I will accept any pull requests that only change these four values:
+exponent = 128
+modulus = 16
+base = 25
+max_values = 30
+
+def terrible_prng(seed = None):
+  # Just in case
+  try:
+    # We'll start with the randomist of numbers: 3.
+    random_numbers = [3]
+    # Make sure the algorithm parameters have the right types. 
+    if not (type(exponent) == int and type(modulus) == int and type(base) == int and type(max_values) == int):
+      return random_numbers
+
+    # You get as many numbers as you get.
+    num_values = random.randint(0, max_values - 1)
+    for _ in range(num_values):
+      base = pow(base, exponent, modulus)
+      random_numbers.append(base)
+    return random_numbers
+  except:
+    return [3]
